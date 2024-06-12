54f1811a7dc27e5307a8435eb520f798f3dea593
wensha2 <shawen@mail.ustc.edu.cn>
2024-06-12 14:36:35 +0800

    update homepage

---
+++ assets/js/son_scripts_1.js
@@ -0,0 +1,11 @@
+function updateTime() {
+    const now = new Date();
+    const timeString = now.toLocaleTimeString('zh-CN', { timeZoneName: 'short' });
+    document.getElementById('time').innerText = timeString;
+}
+
+// 初始化页面时间显示
+updateTime();
+
+// 每秒更新时间
+setInterval(updateTime, 1000);
